dcf16ddefd33639908bcb736f2c099181b045707
Augusto Lima <augustolimads@gmail.com>
2021-04-12 16:22:01 -0300

    update connection config

--- src/database/connection.ts
+++ src/database/connection.ts
@@ -1,11 +1,12 @@
 import knex from 'knex'
+require("dotenv").config();
 
 const connection = knex({
     client: 'mysql2',
     connection: {
-      host : '127.0.0.1',
-      user : 'root',
-      password : process.env.DBSECRET,
+      host : process.env.MYSQL_HOST,
+      user : process.env.MYSQL_ADMIN_USERNAME,
+      password : process.env.MYSQL_ADMIN_PASSWORD,
       database : 'database-name'
     }
   });
